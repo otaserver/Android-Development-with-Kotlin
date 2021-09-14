3daf6e148183c5dfcdd2cde597cd5fc6847463bd
otastudio <qvis.be@gmail.com>
2021-09-14 11:54:24 +0800

    第七章修改代码

--- src/main/kotlin/Chapter07/Method234.kt
+++ src/main/kotlin/Chapter07/Method234.kt
@@ -8,7 +8,7 @@ fun main() {
 
 
     var power: Int.(Int) -> Int
-    //定义power变量是一个扩展函数。扩展的是Int，
+    //定义power变量是一个扩展函数。扩展的是Int，  函数类型，接收Int为参数，返回Int
 
     power = { n -> (1..n).fold(1) { acc, _ -> this * acc } }
     //解析：（type of parameters）-> return type  函数类型。
@@ -17,6 +17,6 @@ fun main() {
     power = fun Int.(n: Int) = (1..n).fold(1) { acc, _ -> this * acc }
     //TODO：看不懂。
 
-    val result = 10.power(3)
+    val result = 10.power(2)
     println(result)
 }
