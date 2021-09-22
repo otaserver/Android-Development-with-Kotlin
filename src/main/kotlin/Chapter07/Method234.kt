fc0e3050436bf7da6de55c0a6346d527f1fb60c6
otastudio <qvis.be@gmail.com>
2021-09-22 10:39:35 +0800

    增加注释

--- src/main/kotlin/Chapter07/Method234.kt
+++ src/main/kotlin/Chapter07/Method234.kt
@@ -3,20 +3,19 @@ package Chapter07
 /**
  * @author scott
  * @date 2021/9/13
+ * 包含接收者的函数类型，或包含接收者的函数字面值。
  */
 fun main() {
 
 
-    var power: Int.(Int) -> Int
+    var power: Int.(Int) -> Int = { n -> (1..n).fold(1) { acc, _ -> this * acc } }
     //定义power变量是一个扩展函数。扩展的是Int，  函数类型，接收Int为参数，返回Int
-
-    power = { n -> (1..n).fold(1) { acc, _ -> this * acc } }
     //解析：（type of parameters）-> return type  函数类型。
 
     //演示包含接收者的函数
-    power = fun Int.(n: Int) = (1..n).fold(1) { acc, _ -> this * acc }
+    var power2 = fun Int.(n: Int) = (1..n).fold(1) { acc, _ -> this * acc }
     //TODO：看不懂。
 
-    val result = 10.power(2)
+    val result = 3.power(2)
     println(result)
 }
